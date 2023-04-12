77eb98dc023d275e972c03eee1a5b8feb79aa0bb
tsahil01 <sahiltiwaskar.21@stvincentngp.edu.in>
2023-04-12 21:38:42 +0530

    #05 create data.js file, made data.js complile with app.js, home.ejs and newblog.ejs working

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const data = require(__dirname + '/data.js');
 
 const app = express();
 app.set('view engine', 'ejs'); // telling our app to use ejs. And we have to store it in views
@@ -10,10 +11,15 @@ app.use(express.static("public")); // to use css and other things
 
 
 app.get("/",function(req,res){
-    res.render('home', {});
+    let details = data.getData();
+    res.render('home', {detailsObejct: details, headingTitle:"Hell Yeah",blogContent:"My Name is Sahil Tiwaskar"});
 })
 app.get("/new",function(req,res){
     res.render('newblog', {});
+    app.post("/new",function(req,res){
+        data.pushData(req.body.heading, req.body.content); //sending new blog details to data.js
+        res.redirect("/");
+    })
 })
 app.get("/blogname",function(req,res){
     res.render('blog', {});
---
+++ data.js
@@ -0,0 +1,14 @@
+// We are having two fn- to get date & to get only day.
+// module.exports is a js object (like JSON)
+// module.exports has shortcut ---> module.exprots = exports
+
+const x = {};
+exports.pushData = function(heading, content){
+    x[heading] = content;
+    // return  x;
+}
+
+exports.getData = function(){
+    return  x;
+}
+
