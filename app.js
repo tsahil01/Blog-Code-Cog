f056a5abd91618137603d9c40555821d6cee166d
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2023-04-21 22:15:55 +0530

    #09 final commit

--- app.js
+++ app.js
@@ -1,52 +1,56 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const ejs = require('ejs');
-const data = require(__dirname + '/data.js');
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const data = require(__dirname + "/data.js");
 const _ = require("lodash");
 
 const app = express();
-app.set('view engine', 'ejs'); // telling our app to use ejs. And we have to store it in views
+app.set("view engine", "ejs"); // telling our app to use ejs. And we have to store it in views
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public")); // to use css and other things
 
 let details = data.getData();
 let detailsKey = Object.keys(details);
-let detailsKeyLowercase = detailsKey.map(word => word.toLowerCase());
+let detailsKeyLowercase = detailsKey.map((word) => word.toLowerCase());
 
+app.get("/", function (req, res) {
+  res.render("home", { detailsObejct: details }); //sends JS object to home
 
-app.get("/",function(req,res){
-    res.render('home', {detailsObejct: details}); //sends JS object to home 
+  app.post("/", function (request, response) {
+    if (Object.keys(details).length === 0) {
+      response.redirect("/new");
+    } else {
+      var h = _.kebabCase(request.body.heading);
+      console.log(request.body.heading);
+      response.redirect("/posts/:${h}");
+    }
+  });
+});
 
-    app.post("/",function(request,response){
+app.get("/new", function (req, res) {
+  res.render("newblog", {});
+  app.post("/new", function (req, res) {
+    var heading = _.upperCase(req.body.heading);
+    var content = req.body.content;
+    data.pushData(heading, content); //sending new blog details to data.js
+    res.redirect("/");
+  });
+});
+app.get("/posts/:postName", function (req, res) {
+  var detailsKeyLowercase = Object.keys(details).map((word) =>
+    word.toLowerCase()
+  );
+  var requestedHeading = _.lowerCase(req.params.postName);
 
-        if(Object.keys(details).length ===0){
-            response.redirect("/new");
-        }
-        
-        else{
-            var h = _.kebabCase(request.body.heading);
-            console.log(request.body.heading)
-            response.redirect("/posts/:${h}");
-        }
+  if (detailsKeyLowercase.includes(requestedHeading)) {
+    res.render("blog", {
+      title: _.upperCase(req.params.postName),
+      content: details[_.upperCase(req.params.postName)],
     });
-})
+  }
+});
 
-app.get("/new",function(req,res){
-    res.render('newblog', {});
-    app.post("/new",function(req,res){
-        var heading = _.upperCase(req.body.heading);
-        var content = req.body.content;
-        data.pushData(heading,content); //sending new blog details to data.js
-        res.redirect("/");
-    })
-})
-app.get("/posts/:postName",function(req,res){
-    var detailsKeyLowercase = Object.keys(details).map(word => word.toLowerCase());
-    var requestedHeading = _.lowerCase(req.params.postName)
-
-    if(detailsKeyLowercase.includes(requestedHeading)){res.render('blog', {title: _.upperCase(req.params.postName), content: details[_.upperCase(req.params.postName)]});} 
-})
-
-
-app.listen(3000,function(){console.log("Server running at port 3000!")});
+app.listen(3000, function () {
+  console.log("Server running at port 3000!");
+});
